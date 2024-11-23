4bc9810c6dee3e9e052d2494873529f8ba2d7402
iankudinov <iankudinov@diasoft.ru>
2024-11-23 19:44:02 +0500

    HomeWork 2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,7 +2,6 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -16,38 +15,72 @@ func Unpack(strIn string) (string, error) {
 		return "", nil
 	}
 
-	// Проверяем, что первый символ это число
+	// Проверяем, что первый символ это число, иначе ошибка
 	_, err := strconv.Atoi(string(strIn[0]))
 	if err == nil { // Если нет ошибки, значит это число
 		return "", ErrInvalidString
 	}
 
-	// Преобразуем строку в срез runes
+	// Преобразуем строку в массив runes, так как легче отличить литерал от экранирование символа в строке
 	runes := []rune(strIn)
 
 	var bufferStr strings.Builder
 	var resultStr strings.Builder
-	countNumericRune := 0
+	lastRuneIsDigital := false
+	lastRuneIsLiteral := false
 
-	// Выводим содержимое runes
-	for num, value := range runes {
-		fmt.Printf("Индекс: %d, Символ: %c, Код: %d\n", num, value, value)
+	for _, value := range runes {
+		// проверка, является ли символ строки целым числом
+		num, err := strconv.Atoi(string(value))
 
-		_, err := strconv.Atoi(string(value))
-		if err != nil {
-			bufferStr.WriteRune(value)
-			countNumericRune = 0
-		} else {
-			countNumericRune++
-			fmt.Printf(" num= %d, value =  %c, intValue = %d, rune = %d\n", num, value, int(value-'0'), countNumericRune)
+		// если цифра повторяется два раза подряд, то ошибка
+		if err == nil && lastRuneIsDigital == true {
+			return "", ErrInvalidString
+		}
 
-			// если предыдущий символ был цифрой, то сообщаем об ошибке
-			if countNumericRune > 1 {
-				return "", ErrInvalidString
+		// был ли сохранен предыдущий символ в буфере
+		if bufferStr.Len() > 0 {
+			// является ли символ числом, да то ..., иначе ...
+			if err == nil {
+				// проверка является ли это одинарным литералом
+				if bufferStr.String() == "\\" && lastRuneIsLiteral == false {
+					bufferStr.Reset()
+					bufferStr.WriteRune(value)
+					continue
+				}
+
+				// сохраняем в буфере с повторением num раз
+				resultStr.WriteString(strings.Repeat(bufferStr.String(), num))
+				bufferStr.Reset()
+
+				lastRuneIsDigital = true  // флаг, что предыдущий символ является число
+				lastRuneIsLiteral = false // флаг, что предыдущий не равен `\`
+			} else {
+				// обнуляем литерал, если он задвоился
+				if bufferStr.String() == "\\" {
+					bufferStr.Reset()
+					bufferStr.WriteRune(value)
+					lastRuneIsLiteral = true
+					continue
+				}
+
+				// сохраняем в буфере с повторением 1 раз
+				resultStr.WriteString(strings.Repeat(bufferStr.String(), 1))
+				bufferStr.Reset()
+
+				bufferStr.WriteRune(value) // сохраняем в буффер текущий символ
+				lastRuneIsDigital = false  // флаг, что предыдущий символ является число
 			}
-			resultStr.WriteString(strings.Repeat(bufferStr.String(), int(value-'0')))
-			bufferStr.Reset()
+		} else {
+			bufferStr.WriteRune(value)
+			lastRuneIsDigital = false
 		}
 	}
+
+	// проверка на последний символ
+	if bufferStr.Len() > 0 {
+		resultStr.WriteString(strings.Repeat(bufferStr.String(), 1))
+	}
+
 	return resultStr.String(), nil
 }
