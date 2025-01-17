f2d9c902575ceedb635b947de494f39bc48ba2a6
iankudinov <iankudinov@diasoft.ru>
2025-01-17 23:45:21 +0500

    HomeWork 6 Correction of mistakes. part2

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,29 +9,57 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	outStream := in
+
 	// Цикл проходит по каждому этапу в списке функций stages
 	for _, stage := range stages {
-		// Обернули в анонимная функция fn, которая принимает входной канал in.
-		// Вызывает текущий этап stage с входным каналом in и получает выходной канал out.
-		fn := func(in In) Out {
-			out := stage(in)
-			outStream := make(Bi)
-
-			go func() {
-				defer close(outStream)
-
-				for v := range out {
-					select {
-					case outStream <- v: // чтение канал (результат текущий этап stage)
-					case <-done:
-						return // Сигнальный канал done используется для остановки пайплайна.
-					}
-				}
-			}()
-			return outStream
-		}
-
-		in = fn(in) // Это означает, что выходной канал текущего этапа становится входным каналом для следующего этапа.
+		outStream = runStage(outStream, done, stage) // Это означает, что выходной канал текущего этапа становится входным каналом для следующего этапа.
 	}
-	return in
+	return outStream
+}
+
+func runStage(in In, done In, stage Stage) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case <-done: // Сигнальный канал done используется для остановки пайплайна.
+				return
+			case value, ok := <-in:
+				if !ok {
+					return
+				}
+				// Запускаем стейдж. Передает канал с данными в стейдж, который читает из него данные и возвращает результат.
+				stageOut := stage(valueToChannel(value, done))
+
+				// Передаем результат в выходной канал
+				select {
+				case out <- <-stageOut:
+				case <-done:
+					return
+				}
+			}
+		}
+	}()
+
+	return out
+}
+
+// Создает новый канал, в который отправляется значение value
+func valueToChannel(value interface{}, done <-chan interface{}) In {
+	channel := make(Bi)
+
+	go func() {
+		defer close(channel)
+
+		select {
+		case channel <- value:
+		case <-done: // Сигнальный канал done используется для остановки пайплайна.
+			return
+		}
+	}()
+	return channel
 }
